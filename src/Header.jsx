16abb3b924d56dba8b37e8b4d1b79b6c66ce7759
Alex <atlexeide@gmail.com>
2025-01-20 14:44:15 +0100

    Update header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,10 @@
 import "./Header.css";
-import NavLinkExternal from "./NavLinkExternal";
 import SearchBar from "./SearchBar";
 function Header({ title }) {
   return (
     <header>
       <h1>{title}</h1>
       <SearchBar />
-      <nav>
-        <ul>
-          <NavLinkExternal
-            link="https://github.com/ATlexEide/KHFEU04-API"
-            text="Repo"
-          />
-          <NavLinkExternal link="https://ponyapi.net/" text="API" />
-        </ul>
-      </nav>
     </header>
   );
 }
