a2d625025b22f023cac988167f95f081cead515f
Alex <atlexeide@gmail.com>
2025-01-20 10:42:24 +0100

    Add header

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+function Header() {
+  return (
+    <header>
+      <h1>To Be Named</h1>
+      <nav>
+        <ul>
+          <li>Repo</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
