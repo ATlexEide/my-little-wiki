a0a500ecec1779e48affef8508d412983913aadc
Alex <atlexeide@gmail.com>
2025-01-20 17:03:01 +0100

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,10 @@ import SearchBar from "./SearchBar";
 function Header({ title }) {
   return (
     <header>
+      <img
+        src="./src/assets/My_Little_Pony_2020.webp"
+        alt="My little pony 2020 logo"
+      />
       <h1>{title}</h1>
       <SearchBar />
     </header>
