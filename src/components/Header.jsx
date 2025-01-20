e150b0071d30f4552452dd5d5f8b3d07d90a7cde
Alex <atlexeide@gmail.com>
2025-01-20 22:19:35 +0100

    Update image link in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
+import getImageUrl from "../assets/getImageUrl";
 import "./Header.css";
 import SearchBar from "./SearchBar";
 function Header({ title }) {
   return (
     <header>
-      <img
-        src="./src/assets/My_Little_Pony_2020.webp"
-        alt="My little pony 2020 logo"
-      />
+      <img src={getImageUrl("mlp2020", "png")} alt="My little pony 2020 logo" />
       <h1>{title}</h1>
       <SearchBar />
     </header>
