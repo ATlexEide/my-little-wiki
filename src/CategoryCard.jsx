96978120737337c086bf491589904bdb96dc9f7a
Alex <atlexeide@gmail.com>
2025-01-20 16:40:44 +0100

    Update category card

--- src/CategoryCard.jsx
+++ src/CategoryCard.jsx
@@ -1,13 +1,14 @@
 import "./CategoryCard.css";
 function CategoryCard({ title, imagePath, alt }) {
   return (
-    <div>
+    <section>
       <figure>
-        <img width={"30%"} src={`${imagePath}`} alt={alt} />
-        <figcaption></figcaption>
+        <a href="#" className="category-link">
+          <img src={`${imagePath}`} alt={alt} />
+        </a>
       </figure>
       <h2>{title}</h2>
-    </div>
+    </section>
   );
 }
 export default CategoryCard;
