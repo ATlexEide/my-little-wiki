a2c865de0dad96764c0a2745f129cd2cceb4fa78
Alex <atlexeide@gmail.com>
2025-01-20 14:44:41 +0100

    Update footer call

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer";
 import fetchCharacterData from "./data/characterData";
 fetchCharacterData("rainbow");
 function App() {
@@ -8,6 +9,11 @@ function App() {
       <main>
         <Header title="My Little Wiki" />
       </main>
+      <Footer
+        title="A &copy;KodeHode Project"
+        author="Alexander"
+        linkSize="1em"
+      />
     </>
   );
 }
