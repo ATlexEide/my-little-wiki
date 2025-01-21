8561176fd1be4cc19af71eb8d234e5b74181f544
Alex <atlexeide@gmail.com>
2025-01-21 17:49:08 +0100

    Update component

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,20 +1,14 @@
 import "./Characters.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import getCharacterData from "../js/getCharacterData";
 import Search from "../components/Search";
-// import { useState } from "react";
 
-const characters = await getCharacterData();
 function Characters() {
-  // characters.map((el) => console.log(el));
-  // const [characters, setCharacters] = useState(getCharacterData());
-  console.log(characters);
   return (
     <>
       <Header title={"Characters"} />
       <main>
-        <Search characters={characters} />
+        <Search />
       </main>
       <Footer />
     </>
