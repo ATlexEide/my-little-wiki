be9a2552997a4c3c38de15eb2b53d16acf62fb2a
Alex <atlexeide@gmail.com>
2025-01-21 12:14:43 +0100

    Add link to route

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,14 +1,15 @@
 import "./CategoryCard.css";
+import { Link } from "react-router-dom";
 function CategoryCard({ title, imagePath, alt }) {
   return (
-    <a href="#" className="category-link">
+    <Link to={`./${title}`} className="category-link">
       <section>
         <figure>
           <img src={`${imagePath}`} alt={alt} />
         </figure>
         <h2>{title}</h2>
       </section>
-    </a>
+    </Link>
   );
 }
 export default CategoryCard;
