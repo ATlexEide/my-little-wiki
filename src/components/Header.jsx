b0b3baa7925e65c8b3ae1e1497b64e5bd9abae3a
Alex <atlexeide@gmail.com>
2025-01-21 12:36:23 +0100

    Add header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import "./Header.css";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 function Header({ title }) {
   return (
     <header>
-      <img src="mlp2020.png" alt="My little pony 2020 logo" />
+      <Link to="/my-little-wiki/">
+        <img src="mlp2020.png" alt="My little pony 2020 logo" />
+      </Link>
       <h1>My Little Wiki{title ? ` | ${title}` : null}</h1>
       <SearchBar />
     </header>
