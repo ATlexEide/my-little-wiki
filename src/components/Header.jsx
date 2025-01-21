29c2df9f1fa7fa9868372315cfda873cdbc4dffd
Alex <atlexeide@gmail.com>
2025-01-21 11:06:25 +0100

    Fix header image url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-import getImageUrl from "../js/getImageUrl";
 import "./Header.css";
 import SearchBar from "./SearchBar";
 function Header({ title }) {
   return (
     <header>
-      <img src={getImageUrl("mlp2020", "png")} alt="My little pony 2020 logo" />
+      <img src="mlp2020.png" alt="My little pony 2020 logo" />
       <h1>{title}</h1>
       <SearchBar />
     </header>
