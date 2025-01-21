f5efc9212d4ccac53d6470d583833e5cff07df9b
Alex <atlexeide@gmail.com>
2025-01-21 11:17:50 +0100

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header({ title }) {
   return (
     <header>
       <img src="mlp2020.png" alt="My little pony 2020 logo" />
-      <h1>{title}</h1>
+      <h1>My Little Wiki{title ? ` | ${title}` : null}</h1>
       <SearchBar />
     </header>
   );
