e598ec1e8a16d399722e9dea5f974b4d383e31a9
Alex <atlexeide@gmail.com>
2025-01-21 16:08:12 +0100

    Update component logic

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,9 +1,22 @@
+import "./Characters.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import getCharacterData from "../js/getCharacterData";
+import SearchBar from "../components/Search";
+import { useState } from "react";
+
 function Characters() {
+  // const characters = await getCharacterData();
+  // characters.map((el) => console.log(el));
+  const [characters, setCharacters] = useState(getCharacterData());
+  console.log(characters);
   return (
     <>
       <Header title={"Characters"} />
+      <main>
+        <SearchBar />
+        <ul></ul>
+      </main>
       <Footer />
     </>
   );
