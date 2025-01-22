dc204a881039587a86c6bc428294a94f1202ab64
Alex <atlexeide@gmail.com>
2025-01-22 16:03:09 +0100

    Update component

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Search from "../components/Search";
 function Comics() {
   return (
     <>
-      <Header title={"Comics"} />
+      <Header title={"Characters"} />
+      <main id="character-list">
+        <Search category="comics-story" />
+      </main>
       <Footer />
     </>
   );
