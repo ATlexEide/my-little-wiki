fbd33675f2ad93a0fc92b59406aabf7a016d0f1e
Alex <atlexeide@gmail.com>
2025-01-22 15:59:05 +0100

    Add search component to songs page

--- src/pages/Songs.jsx
+++ src/pages/Songs.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Search from "../components/Search";
 function Songs() {
   return (
     <>
       <Header title="Songs" />
+      <main>
+        <Search category="song" />
+      </main>
       <Footer />
     </>
   );
