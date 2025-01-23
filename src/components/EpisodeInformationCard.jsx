93145e1dec9f3bb4686b3637015a088be7025034
Alex <atlexeide@gmail.com>
2025-01-23 18:10:34 +0100

    Add missing info

--- src/components/EpisodeInformationCard.jsx
+++ src/components/EpisodeInformationCard.jsx
@@ -2,9 +2,32 @@ function EpisodeInformationCard({ episode }) {
   return (
     <section>
       {/* EPISODE */}
+      {episode.image && (
+        <figure>
+          <img src={episode.image} alt="" />
+          <h1>{episode.name}</h1>
+          <p>{episode.alias && episode.alias}</p>
+        </figure>
+      )}
+      {!episode.image && <h1>{episode.name}</h1>}
+      {episode.episode && (
+        <p>
+          <b>Episode</b>: {episode.episode}
+        </p>
+      )}
       {episode.season && (
         <p>
           <b>Season</b>: {episode.season}
+        </p>
+      )}
+      {episode.song && (
+        <p>
+          <b>Songs</b>:{" "}
+          {episode.song.length > 1
+            ? episode.song.slice(0, episode.song.length - 1).join(", ") +
+              " and " +
+              episode.song[episode.song.length - 1]
+            : episode.song[0]}
         </p>
       )}
       {episode.overall && (
