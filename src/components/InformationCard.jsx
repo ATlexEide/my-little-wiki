bb44ba0e2474379b8ddb99a33a2a69db51c7590d
Alex <atlexeide@gmail.com>
2025-01-23 16:28:32 +0100

    Add alias and kind

--- src/components/InformationCard.jsx
+++ src/components/InformationCard.jsx
@@ -5,18 +5,29 @@ function InformationCard({ category, id }) {
     fetch(`https://ponyapi.net/v1/${category}/${id}`)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res.data[0]);
         setTarget(res.data[0]);
       })
       .catch((e) => console.log(e));
   }, [category, id]);
+
   return (
     <section>
       <figure>
         <img src={target.image && target.image[0]} alt="" />
       </figure>
       <h1>{target.name}</h1>
-      <p>{target.alias ?? target.alias}</p>
+      <p>{target.alias && target.alias}</p>
+      {target.residence && <p>Residence: {target.residence}</p>}
+      {target.kind && (
+        <p>
+          Kind:{" "}
+          {target.kind.length > 1
+            ? target.kind.slice(0, target.kind.length - 1).join(", ") +
+              " and " +
+              target.kind[target.kind.length - 1]
+            : target.kind[0]}
+        </p>
+      )}
     </section>
   );
 }
