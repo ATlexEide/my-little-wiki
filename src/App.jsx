375c7178b6b4a160fa289411de85fbec61fd7684
Alex <atlexeide@gmail.com>
2025-01-23 16:00:23 +0100

    Add routes for information

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Characters from "./pages/Characters";
 import Songs from "./pages/Songs";
 import Episodes from "./pages/Episodes";
 import Comics from "./pages/Comics";
+import Information from "./pages/Information";
 function App() {
   return (
     <Router>
@@ -14,6 +15,22 @@ function App() {
         <Route path="my-little-wiki/songs" element={<Songs />} />
         <Route path="my-little-wiki/episodes" element={<Episodes />} />
         <Route path="my-little-wiki/comics" element={<Comics />} />
+        <Route
+          path="my-little-wiki/characters/:id"
+          element={<Information category="character" />}
+        />
+        <Route
+          path="my-little-wiki/characters/:id"
+          element={<Information category="song" />}
+        />
+        <Route
+          path="my-little-wiki/characters/:id"
+          element={<Information category="episode" />}
+        />
+        <Route
+          path="my-little-wiki/characters/:id"
+          element={<Information category="comic" />}
+        />
       </Routes>
     </Router>
   );
