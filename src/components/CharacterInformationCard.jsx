cd52a4a939ec1346da88c3acdbfa65db34e53438
Alex <atlexeide@gmail.com>
2025-01-24 13:07:41 +0100

    Refactor

--- src/components/CharacterInformationCard.jsx
+++ src/components/CharacterInformationCard.jsx
@@ -1,52 +1,21 @@
+import Name from "./InfoCardComponents/Name";
+import Residence from "./InfoCardComponents/Residence";
+import Sex from "./InfoCardComponents/Sex";
+import Occupation from "./InfoCardComponents/Occupation";
+import Kind from "./InfoCardComponents/Kind";
+import Images from "./InfoCardComponents/Images";
+import Url from "./InfoCardComponents/Url";
 function CharacterInformationCard({ character }) {
   console.log(character);
   return (
     <section id="infocard">
-      {/* CHARACTERS */}
-      {character.image && (
-        <figure>
-          <img src={character.image && character.image[0]} alt="" />
-          <h1>{character.name}</h1>
-          <p>{character.alias && character.alias}</p>
-        </figure>
-      )}
-      {!character.image && <h1>{character.name}</h1>}
-      {!character.image && <p>{character.alias && character.alias}</p>}
-      {character.sex && (
-        <p>
-          <b>sex</b>:{character.sex}
-        </p>
-      )}
-      {character.residence && (
-        <p>
-          <b>Residence</b>: {character.residence}
-        </p>
-      )}
-      {character.occupation && (
-        <p>
-          <b>Occupation</b>: {character.occupation}
-        </p>
-      )}
-      {character.kind && (
-        <p>
-          Kind:{" "}
-          {character.kind.length > 1
-            ? character.kind.slice(0, character.kind.length - 1).join(", ") +
-              " and " +
-              character.kind[character.kind.length - 1]
-            : character.kind[0]}
-        </p>
-      )}
-      <>
-        {character.image && <h2>More images:</h2>}
-        {character.image &&
-          character.image
-            .filter((img, index) => index > 0)
-            .map((image, index) => {
-              return <img key={image + index} src={image} />;
-            })}
-      </>
-      {character.url && <a href={character.url}>Fandom wiki page</a>}
+      <Name target={character} />
+      <Sex target={character} />
+      <Residence target={character} />
+      <Occupation target={character} />
+      <Kind target={character} />
+      <Images target={character} />
+      <Url target={character} />
     </section>
   );
 }
