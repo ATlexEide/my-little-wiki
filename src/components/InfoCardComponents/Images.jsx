5c7894525bc3913a5e6c780f276509d0ec848dd2
Alex <atlexeide@gmail.com>
2025-01-24 16:26:35 +0100

    Add ids

--- src/components/InfoCardComponents/Images.jsx
+++ src/components/InfoCardComponents/Images.jsx
@@ -1,14 +1,16 @@
 function Images({ target }) {
   return (
-    <>
+    <div id="gallery">
       {target.image && <h2>More images:</h2>}
-      {target.image &&
-        target.image
-          .filter((img, index) => index > 0)
-          .map((image, index) => {
-            return <img key={image + index} src={image} />;
-          })}
-    </>
+      <div id="images">
+        {target.image &&
+          target.image
+            .filter((img, index) => index > 0)
+            .map((image, index) => {
+              return <img key={image + index} src={image} />;
+            })}
+      </div>
+    </div>
   );
 }
 export default Images;
