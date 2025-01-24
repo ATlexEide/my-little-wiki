3b0224c7932f341fc1719aa77ed1e1eed04c6479
Alex <atlexeide@gmail.com>
2025-01-24 16:27:14 +0100

    Fix missing return

--- src/components/InfoCardComponents/Residence.jsx
+++ src/components/InfoCardComponents/Residence.jsx
@@ -1,10 +1,10 @@
 function Residence({ target }) {
-  {
+  return (
     target.residence && (
       <p>
         <b>Residence</b>: {target.residence}
       </p>
-    );
-  }
+    )
+  );
 }
 export default Residence;
