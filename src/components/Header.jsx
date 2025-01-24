1f1e92ef3ea2aded8b43ba5e8197e3c950ef141e
Alex <atlexeide@gmail.com>
2025-01-24 16:44:57 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,7 @@ function Header({ title }) {
   return (
     <header>
       <Link to="/my-little-wiki/">
-        <img
-          src="/my-little-wiki/public/mlp2020.png"
-          alt="My little pony 2020 logo"
-        />
+        <img src="public/mlp2020.png" alt="My little pony 2020 logo" />
       </Link>
       <h1>My Little Wiki{title ? ` | ${title}` : null}</h1>
     </header>
