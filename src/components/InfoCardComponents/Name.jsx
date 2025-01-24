5c57c696c198e3b521380a17513e0b3219227e8a
Alex <atlexeide@gmail.com>
2025-01-24 16:27:01 +0100

    Add ids

--- src/components/InfoCardComponents/Name.jsx
+++ src/components/InfoCardComponents/Name.jsx
@@ -2,14 +2,18 @@ function Name({ target }) {
   return (
     <>
       {target.image && (
-        <figure>
-          <img src={target.image && target.image[0]} alt={target.name} />
-          <h1>{target.name}</h1>
-          <p>{target.alias && target.alias}</p>
+        <figure id="info-figure">
+          <img
+            id="figure-image"
+            src={target.image && target.image[0]}
+            alt={target.name}
+          />
+          <h1 id="figure-header">{target.name}</h1>
+          {target.alias && <p>{target.alias}</p>}
         </figure>
       )}
       {!target.image && <h1>{target.name}</h1>}
-      {!target.image && <p>{target.alias && target.alias}</p>}
+      {!target.image && target.alias && <p>{target.alias && target.alias}</p>}
     </>
   );
 }
