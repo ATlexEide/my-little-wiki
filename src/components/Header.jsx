f0b51a44ad811b70237ab4e762872bcc1fe6516e
Alex <atlexeide@gmail.com>
2025-01-24 16:37:39 +0100

    Fix image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ function Header({ title }) {
   return (
     <header>
       <Link to="/my-little-wiki/">
-        <img src="mlp2020.png" alt="My little pony 2020 logo" />
+        <img
+          src="/my-little-wiki/public/mlp2020.png"
+          alt="My little pony 2020 logo"
+        />
       </Link>
       <h1>My Little Wiki{title ? ` | ${title}` : null}</h1>
     </header>
