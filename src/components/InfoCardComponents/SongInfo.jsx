b97d32fe3848a9353a35d900a5dfb56735913a3d
Alex <atlexeide@gmail.com>
2025-01-26 18:42:55 +0100

    Add SongInfo component

---
+++ src/components/InfoCardComponents/SongInfo.jsx
@@ -0,0 +1,28 @@
+function SongInfo({ target }) {
+  return (
+    <>
+      {target.episode && (
+        <p>
+          <b>Episode</b>
+          {target.episode}
+        </p>
+      )}
+      {target.musicby && (
+        <p>
+          <b>Music by</b>:{target.musicby}
+        </p>
+      )}
+      {target.lyricsby && (
+        <p>
+          <b>Lyrics by</b>:{target.lyricsby}
+        </p>
+      )}
+      {target.keysignature && (
+        <p>
+          <b>Key signature</b>:{target.keysignature}
+        </p>
+      )}
+    </>
+  );
+}
+export default SongInfo;
