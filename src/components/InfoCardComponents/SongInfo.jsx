ba59d151300f9c4a6feb3012e844413fa71bdf06
Alex <atlexeide@gmail.com>
2025-01-28 17:59:56 +0100

    Fix missing semi-colon and space

--- src/components/InfoCardComponents/SongInfo.jsx
+++ src/components/InfoCardComponents/SongInfo.jsx
@@ -3,23 +3,22 @@ function SongInfo({ target }) {
     <>
       {target.episode && (
         <p>
-          <b>Episode</b>
-          {target.episode}
+          <b>Episode</b>: {target.episode}
         </p>
       )}
       {target.musicby && (
         <p>
-          <b>Music by</b>:{target.musicby}
+          <b>Music by</b>: {target.musicby}
         </p>
       )}
       {target.lyricsby && (
         <p>
-          <b>Lyrics by</b>:{target.lyricsby}
+          <b>Lyrics by</b>: {target.lyricsby}
         </p>
       )}
       {target.keysignature && (
         <p>
-          <b>Key signature</b>:{target.keysignature}
+          <b>Key signature</b>: {target.keysignature}
         </p>
       )}
     </>
